77e10ea8cacfc9972006e32b08ff715d8618312d
Geovannapg12 <geo.p2334@gmail.com>
2022-06-06 21:44:11 -0700

    assessment finished

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,8 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+const { exportAllDeclaration } = require("@babel/types")
+const { array } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -13,7 +16,25 @@
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
+
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+
+
+describe ("kolors", () =>  {
+it ("returns an array, removes the first item from the array and shuffles the remaining content." , () => {
+    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+     const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+     expect(kolors(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+     expect(kolors(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+})
+})
+
+// ● kolors › returns an array, removes the first item from the array and shuffles the remaining content.
+
+// ReferenceError: kolors is not defined
+
+
+
 
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
@@ -23,10 +44,37 @@ const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "s
 
 // b) Create the function that makes the test pass.
 
+//Create a function named kolors that will shuffle an array and remove the first item
+// Use the method .shift to remove the first item of the array
+// Return the array with the method sort to sort all the values insde of the array
+/////// The method sort accepts a function that returns a value between -0.5 and 0.5
+
+
+const kolors = (array) => {
+  array.shift()
+      return array.sort((a, b) => Math.random() - 0.6)
+}
+
+console.log(kolors(colors1))
+
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe ("minMaxArr", () => {
+    it ("returns array of numbers and returns an array of the minimum and maximum numbers in that order.", ( ) => {
+        const nums1 = [3, 56, 90, -8, 0, 23, 6]
+        const nums2 = [109, 5, 9, 67, 8, 24]
+        expect(minMaxArr(nums1)).toEqual([-8, 90])
+        expect(minMaxArr(nums2)).toEqual([5, 109])
+    })
+    
+})
+
+//// ● minMaxArr › returns array of numbers and returns an array of the minimum and maximum numbers in that order.
+
+//// ReferenceError: minMaxArr is not defined
 
 const nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
@@ -36,10 +84,39 @@ const nums2 = [109, 5, 9, 67, 8, 24]
 
 // b) Create the function that makes the test pass.
 
+//Creacte a function named minMaxArr that will take the arry of numbers and return the numbers from slamments to greatest.
+//Create an empty array to store the numbers 
+//In the new created array use method .push to be able to use the Math.min and max into the new array
+//return the  new array with the methods
+
+
+const minMaxArr = (array) => {
+    let newArray = []
+  newArray.push(Math.min(...array))
+  newArray.push(Math.max(...array))
+    return newArray
+}
+
+console.log(minMaxArr(nums1))
+console.log(minMaxArr(nums2))
+
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
+
+describe ("noDuplicate", () => {
+    it ("returns two arrays as arguments and returns one array with no duplicate values", () => { 
+        const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+        const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+        expect(noDuplicate(testArray1,testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1]);
+    }) 
+})
+
+//// ● noDuplicate › returns two arrays as arguments and returns one array with no duplicate values
+
+//// ReferenceError: noDuplicate is not defined
+
 
 const testArray1 = [3, 7, 10, 5, 4, 3, 3]
 const testArray2 = [7, 8, 2, 3, 1, 5, 4]
@@ -47,3 +124,17 @@ const testArray2 = [7, 8, 2, 3, 1, 5, 4]
 
 
 // b) Create the function that makes the test pass.
+
+// Create a function named  noDuplicate
+// Use method .concat to join the two arrays and make it into a new one 
+// Have the function return the new array with the -set- (I found [...new Set in stack overflow, what it does is that Set can only contain unique values, and the spread operator converts it back into an array)
+// console log the arrays
+
+
+const noDuplicate = (array1, array2) => {
+joinArray = array1.concat(array2)
+return joinArray = [...new Set(joinArray)]
+
+} 
+
+console.log(noDuplicate(testArray1, testArray2))
--- interview-questions.md
+++ interview-questions.md
@@ -7,52 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What are props in React?
 
-  Your answer:
+  Your answer: props stands for properties. What it does in react is that it passes information from one component to another.
 
-  Researched answer:
+  Researched answer: Since React is component based there needs to be communication between components. That is where props helps, it is used as a way to pass information and methods between components and it gives us the ability to pass data to nested components.
 
 
 
 2. What is a DOM event?
 
-  Your answer:
+  Your answer: A DOM event happens whenever the user interacts with the web page. For example, if there is a game of tic tac toe a DOM event happens every time we click on the little box and it returns an x or an o. Or when the user types their email, or when they hover on a name and it changes color. 
 
-  Researched answer:
+  Researched answer: Web applications need to be able to interact with users. When the user clicks, hovers over elements and type. DOM waits to get the interaction and runs the change. 
 
 
 
 3. What is object-oriented programming? How is it different than functional programming?
 
-  Your answer:
+  Your answer: It means it focuses mainly on classes and objects, like Ruby. I am not completely sure of what functional programming is. But looking at the name I believe it focuses on functions rather than objects. 
 
-  Researched answer:
+  Researched answer: Functional programming relies on fucntions, object-oriented programming is based on classes and respective objects. 
+
 
 
 
 4. What is the difference between a Float and an Integer in Ruby?
 
-  Your answer:
+  Your answer: A float is not a whole number, a decimal number 10.4 .An integer is a whole number like 13. 
 
-  Researched answer:
+  Researched answer: In Ruby an integer is a whole number and it can be negative number. A float is a number followed by a decimal point
 
 
 
 5. Ruby has an implicit return. What does that mean?
 
-  Your answer:
+  Your answer: So I was not exactly sure of what this one meant. After googling implicit I got "implied though not plainly expressed. It is obviously a difference between Ruby and JavaScript, right now all I can think about is that maybe there is no need for parenthesis and at some points curly brakets or the arrow. 
 
-  Researched answer:
+  Researched answer: So what it means is that in Ruby the value of the last line of a method is automatically returnes without having to have a return in there. It is not expressed and not necessary. 
 
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Instance Variable:
+1. Instance Variable: When a variable starts with an @ is an instance variable, that means it belongs to an intance of a class but is outside of constructors, methods, or blocks.
 
-2. PostgreSQL:
+2. PostgreSQL: Is an object-relational database managment system. It is very stable and is used as the primary data store warehouse for many web, mobile and analytic applicatons. 
 
-3. Ruby on Rails:
+3. Ruby on Rails: Is a server-side web application framework written in ruby. It provides default structures for a database, web service and web pages. 
 
-4. ORM:
+4. ORM: Object-Relational Mapping. Is a programming technique for converting data between incompatible systems (if one type of computer or computer system is incompatible with another, they cannot use the same programs or be linked up together) using object oriented programming languages. 
 
-5. Active Record:
+5. Active Record: Is a rails ORM. It takes data that is stored in a database table and lets you interact with the data like a Ruby object,
